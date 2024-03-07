1e1fffd26eba217364b0ff771c373128e0ad8c31
amr <amr.soubhi.hassanieen@gmail.com>
2024-03-07 16:48:43 +0200

    grgkgtl;kl;tkl;bg

--- lib/cubits/app_cupit.dart
+++ lib/cubits/app_cupit.dart
@@ -3,10 +3,11 @@ import '/cubits/cupit_states.dart';
 
 class app_cupit extends Cubit<cupit_states> {
   static String lan = 'en';
+  static String lfffan = 'en';
 ///////////////////////////
-///ghghg
-///hghg
-///h
+  ///ghghg
+  ///hghg
+  ///h
   app_cupit() : super(initialState()) {
     emit(onboardingPageState());
   }
